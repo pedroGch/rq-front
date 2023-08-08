114460c57fab83719ed40f33466aa81bdfeb9817
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-08-08 01:21:12 -0300

    habilidad comunicacion

--- src/components/habilidades/HabilidadComunicacion.vue
+++ src/components/habilidades/HabilidadComunicacion.vue
@@ -1,19 +1,19 @@
 <template>
   <div> 
-    <h3 class="text-center tex-bold bg-blue-300 p-0">Agilidad (10)</h3>
+    <h3 class="text-center tex-bold bg-blue-300 p-0">Comunicación (10)</h3>
     <table class="min-w-full divide-y divide-gray-200">
       <tbody class="p-2 bg-white divide-y divide-gray-200">
         <!-- Fila 1 -->
         <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Arrojar</td>
-          <td class="px-6 py-4 whitespace-nowrap">25</td>                        
+          <td class="px-6 py-4 whitespace-nowrap">Actuar</td>
+          <td class="px-6 py-4 whitespace-nowrap">5</td>                        
           <td class="px-6 py-4 whitespace-nowrap">
             <input type="checkbox">
           </td>
         </tr>
 
         <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Esquivar</td>
+          <td class="px-6 py-4 whitespace-nowrap">Cantar</td>
           <td class="px-6 py-4 whitespace-nowrap">5</td>
           <td class="px-6 py-4 whitespace-nowrap">
             <input type="checkbox">
@@ -21,7 +21,7 @@
         </tr>
 
         <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Montar</td>
+          <td class="px-6 py-4 whitespace-nowrap">Habla fluida</td>
           <td class="px-6 py-4 whitespace-nowrap">5</td>
           <td class="px-6 py-4 whitespace-nowrap">
             <input type="checkbox">
@@ -29,7 +29,15 @@
         </tr>
 
         <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Nadar</td>
+          <td class="px-6 py-4 whitespace-nowrap">Idioma propio</td>
+          <td class="px-6 py-4 whitespace-nowrap">30</td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <input type="checkbox">
+          </td>
+        </tr>
+
+        <tr>
+          <td class="px-6 py-4 whitespace-nowrap">Oratoria</td>
           <td class="px-6 py-4 whitespace-nowrap">5</td>
           <td class="px-6 py-4 whitespace-nowrap">
             <input type="checkbox">
@@ -37,7 +45,7 @@
         </tr>
 
         <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Remar</td>
+          <td class="px-6 py-4 whitespace-nowrap">Regatear</td>
           <td class="px-6 py-4 whitespace-nowrap">5</td>
           <td class="px-6 py-4 whitespace-nowrap">
             <input type="checkbox">
@@ -45,23 +53,7 @@
         </tr>
 
         <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Saltar</td>
-          <td class="px-6 py-4 whitespace-nowrap">5</td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <input type="checkbox">
-          </td>
-        </tr>
-
-        <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Trepar</td>
-          <td class="px-6 py-4 whitespace-nowrap">40</td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <input type="checkbox">
-          </td>
-        </tr>
-
-        <tr>
-          <td class="px-6 py-4 whitespace-nowrap">Usar velero</td>
+          <td class="px-6 py-4 whitespace-nowrap">Seducción</td>
           <td class="px-6 py-4 whitespace-nowrap">5</td>
           <td class="px-6 py-4 whitespace-nowrap">
             <input type="checkbox">
