5de60a596c3f3eb03991d9c6906a66461699908e
pedro <pedro.gch01@gmail.com>
2023-08-10 19:33:31 -0300

    imagen al 100% del ancho se agregan dos datos mas

--- src/components/InfoGeneral.vue
+++ src/components/InfoGeneral.vue
@@ -1,14 +1,24 @@
 <template>
-  <section class="w-[375px] mt-[28px] px-[27px] flex">
-    <img class="w-[50%]" src="/pantalla_inicio_sesion.jpg" alt="">
-    <ul class="w-[50%] ml-[10px]">
-      <li class="mb-[5px] pl-[2px] bg-gray-400">Pto Magicos: 12</li>
-      <li class="mb-[5px]">Pto Fatiga: 12</li>
-      <li class="mb-[5px] pl-[2px] bg-gray-400">Pto Golpe: 12</li>
-      <li class="mb-[5px]">MMR-CC: 12</li>
-      <li class="mb-[5px] pl-[2px] bg-gray-400">MMR-TAM: 12</li>
-      <li class="mb-[5px]">MMR-DES: 12</li>
-      <li><button class="rounded-md w-[100%] bg-azul text-white text-center mt-[16px]" type="button">Ver más +</button></li>
-    </ul>
+  <section class="w-[375px] mt-[28px] px-[27px] ">
+    <div class="w-[100%]">
+      <img class="" src="/pantalla_inicio_sesion.jpg" alt="">
+    </div>
+    <div class="mt-[10px] flex justify-center items-center">
+      <ul class="grid grid-cols-2 gap-5 ">
+        <li class="mb-[5px] pl-[2px] ">Pto Magicos: 12</li>
+        <li class="mb-[5px]">Pto Fatiga: 12</li>
+        <li class="mb-[5px] pl-[2px] ">Pto Golpe: 12</li>
+        <li class="mb-[5px]">MMR-CC: 12</li>
+        <li class="mb-[5px] pl-[2px] ">MMR-TAM: 12</li>
+        <li class="mb-[5px]">MMR-DES: 12</li>
+        <li class="mb-[5px] pl-[2px] ">MMR-TAM: 12</li>
+        <li class="mb-[5px]">MMR-DES: 12</li>
+        <li class="mb-[5px] pl-[2px] ">MMR-TAM: 12</li>
+        <li class="mb-[5px]">MMR-DES: 12</li>
+      </ul>
+    </div>
+    <div class="w-[100%] m-0-auto">
+      <button class="rounded-md w-[100%] bg-azul text-white text-center mt-[16px]" type="button">Ver más +</button>
+    </div>
   </section>
 </template>
