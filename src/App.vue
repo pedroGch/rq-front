67ce8fd1e70c0b36a9caedd1dcfc472c05a30381
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-09-02 21:08:12 -0300

    agrego componente navapp

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 
 <template>
   <div>
+    <nav-app></nav-app>
     <section class=" flex justify-center pt-[28px]">
       <span class="w-[264px] h-[98px] bg-encabezado bg-no-repeat flex items-center pt-[10px]">
         <h1 class="w-[100%] text-center align-center text-[20px] fondo-texto-verde">
@@ -41,6 +42,9 @@ export default {
     return{
       nombrePersonaje: "AKAZA",
     }
+  },
+  mounted(){
+    
   }
 }
 </script>
